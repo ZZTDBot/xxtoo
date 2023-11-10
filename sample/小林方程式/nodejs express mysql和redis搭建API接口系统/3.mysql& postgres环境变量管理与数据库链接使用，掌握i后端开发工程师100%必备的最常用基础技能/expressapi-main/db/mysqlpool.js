f833d562c5831bd8e6d12466e38d9824243498f2
ZZJQR <shengcai.ton@gmail.com>
2023-11-10 09:04:23 -0800

    main

--- sample/小林方程式/nodejs express mysql和redis搭建API接口系统/3.mysql& postgres环境变量管理与数据库链接使用，掌握i后端开发工程师100%必备的最常用基础技能/expressapi-main/db/mysqlpool.js
+++ sample/小林方程式/nodejs express mysql和redis搭建API接口系统/3.mysql& postgres环境变量管理与数据库链接使用，掌握i后端开发工程师100%必备的最常用基础技能/expressapi-main/db/mysqlpool.js
@@ -23,8 +23,15 @@ console.log("Created a connection pool with 10 active connections to MySQL");
 
 const initDb = async () =>{
     try{
+        /**
+         * await pool.getConnection() 是手动获取一个数据库的链接方法 如果不使用这个语句 pool将自动分配一个链接给每个query 
+         * 而使用getConnection()方法 可以让多个query使用一个固定的数据库连接，这个在事务控制的时候非常有用 
+         */
         let connection = await pool.getConnection();
         console.log("Manually acquired a connection from the pool");
+        /**
+         * mysqlpool.js文件创建的表名是customers,不过表结构和users表是一样的
+         */
         let createtableresult = await connection.execute("CREATE TABLE IF NOT EXISTS customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), email VARCHAR(255))");
         console.log("Created table customers");
         let insertresult = await connection.execute("INSERT INTO customers( name , email ) values ( 'Tom' , 'tom@test.com'),( 'Jerry' , 'jerry@test.com');");
